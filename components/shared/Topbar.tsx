5b0796e444fac8322ed6897c09c120325cc45860
Ayomide <ayomidedans@gmail.com>
2024-04-22 20:18:49 +0100

    feat: added relevants assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
   return (
-    <h1>Top bar</h1>
+    <nav className="topbar">
+      <Link href={'/'} className="flex items-center gap-4">
+        <Image src={"/assets/logo.svg"} alt="logo" width={28} height={18} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Neon Threads</p>
+      </Link>
+    </nav>
   );
 }
 
