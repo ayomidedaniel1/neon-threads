36b6d45db2179326d74152cb7defb16076926dd1
Ayomide <ayomidedans@gmail.com>
2024-05-17 01:16:34 +0100

    chore: type insurance for user actions

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { useUploadThing } from '@/lib/uploadthing';
 import { isBase64Image } from '@/lib/utils';
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -86,7 +90,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    // TODO: Update user profile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname
+    });
   };
 
   return (
