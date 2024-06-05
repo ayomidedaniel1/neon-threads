394bd9473f0d889e520da610887ca1a493916fbb
Ayomide <ayomidedans@gmail.com>
2024-06-05 11:31:05 +0100

    feat: added shadcn tabs

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 interface Props {
   accountId: string;
@@ -10,8 +11,30 @@ interface Props {
 
 const ProfileHeader = ({ accountId, authUserId, name, username, imgUrl, bio, }: Props) => {
   return (
-    <div>
-      <h1>Profile Header</h1>
+    <div className="flex w-full flex-col justify-start">
+      <div className="flex items-center justify-between">
+        <div className='flex items-center gap-3'>
+          <div className="relative h-20 w-20 object-cover">
+            <Image
+              src={imgUrl}
+              alt='logo'
+              fill
+              className='rounded-full object-cover shadow-2xl'
+            />
+          </div>
+
+          <div className="flex-1">
+            <h2 className="text-left text-heading3-bold text-light-1">{name}</h2>
+            <p className="text-base-mediium text-gray-1">{username}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Community Todo */}
+
+      <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
+
+      <div className="mt-12 h-0.5 w-4 bg-dark-3" />
     </div>
   );
 };
